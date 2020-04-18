8c7512d4c124c4b7331d5795bc0aedb812d49859
Damian Zgoda <zgoda91@tlen.pl>
2020-04-18 20:38:52 +0200

    init script fix

--- initialize_project.sh
+++ initialize_project.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ ! -d "/googletest" ]; then
+if [ ! -d "./googletest" ]; then
     echo "Cloning googletest repository..."
     git clone https://github.com/google/googletest
 fi
 
-if [ ! -d "/src/logger" ]; then
+if [ ! -d "./src/logger" ]; then
     echo "Cloning logger repository..."
     cd src
     git clone https://github.com/g3t0r1x/logger
